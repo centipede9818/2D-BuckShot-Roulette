220af2633228645362ed4a21f386f728c22e420f
centipede9818 <centipede9818@gmail.com>
2024-08-14 14:42:28 +0530

    Update player.java

--- src/player/player.java
+++ src/player/player.java
@@ -14,13 +14,14 @@ public class player {
         
     }
     public void update(){
+
     }
 
     public void draw(Graphics2D g2){
         BufferedImage image=player_img;
         g2.drawImage(image,(game_panel.WIDTH-player_x)/2,game_panel.HEIGHT-player_y-margin, player_x, player_y,null);
         // PLAYER
-        g2.fillRect((game_panel.WIDTH-player_x)/2,game_panel.HEIGHT-player_y-margin, player_x, player_y);
+        //g2.fillRect((game_panel.WIDTH-player_x)/2,game_panel.HEIGHT-player_y-margin, player_x, player_y);
     }
 
 }
